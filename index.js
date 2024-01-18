2d255fa2ab5b36743e12f1c915f024acfd2f7202
kartiksam <139067986+kartiksam@users.noreply.github.com>
2024-01-18 20:20:52 +0530

    commit

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
-  res.end("hello from the kartik");
-});
-server.listen(8000, "127.0.0.1", () => {
-  console.log("listening to request on port no 5000");
-});
+console.log("kartik shrama");
