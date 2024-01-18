9fd344451816d73b3b752f9afc61aec78aee00c9
kartiksam <139067986+kartiksam@users.noreply.github.com>
2024-01-18 20:14:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+  res.end("hello from the kartik");
+});
+server.listen(8000, "127.0.0.1", () => {
+  console.log("listening to request on port no 8000");
+});
